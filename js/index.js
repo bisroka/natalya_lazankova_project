559ae28c726a90663faf8f7c4ef6e22ec4d95016
Bartosz <bartosz.sroka4@gmail.com>
2021-03-31 10:21:35 +0200

    Clean code

--- js/index.js
+++ js/index.js
@@ -1,4 +1,3 @@
-// import { animationAboutMe } from "./animationAboutMe"
 import { animationHeader } from "./animationHeader"
 import { animationContact } from "./animationContact"
 import { animationGallery } from "./animationGallery"
@@ -6,16 +5,9 @@ import { animationAboutMe } from "./animationAboutMe"
 import { masonryLayout } from "./masonry"
 import { ctaHandler } from "./cta"
 
-// animationAboutMe()
 animationHeader()
 animationContact()
 animationGallery()
 animationAboutMe()
 masonryLayout()
 ctaHandler()
-
-// import('./animationAbouMe').then(animationAboutMe())
-// import('./animationHeader').then(animationHeader())
-// import('./animationContact').then(animationContact())
-// import('./animationGallery').then(animationGallery())
-// import('./masonry').then(masonryLayout())
